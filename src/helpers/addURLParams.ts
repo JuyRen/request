6cd4922d67d2525f064d782b4a252991b47f2db2
renjunyang <juyren@163.com>
2021-06-22 17:42:24 +0800

    chore: 修改package.json files

--- src/helpers/addURLParams.ts
+++ src/helpers/addURLParams.ts
@@ -4,7 +4,7 @@ function addURLParams<T extends Data['GET']>(url: string, data?: T): string {
     if (!data) return url;
 
     for (const [key, value] of Object.entries(data)) {
-        if (typeof value !== 'undefined') {
+        if (typeof value !== 'undefined' && value !== null) {
             url += url.indexOf('?') === -1 ? '?' : '&';
             url += `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
         }
